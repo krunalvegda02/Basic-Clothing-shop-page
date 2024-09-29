69adb9a77c1b704bb5ee20558ba59c1639728a8d
krunalvegda02 <Krunal.vegda02@gmail.com>
2024-09-29 19:37:26 +0530

    final commit

--- WEB TECHNOLOGY/script.js
+++ WEB TECHNOLOGY/script.js
@@ -5,15 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const reviewList = document.getElementById('reviewList');
     const submitReviewBtn = document.getElementById('submitReview');
 
-    // submitRatingBtn.addEventListener('click', function () {
-    //     const rating = ratingInput.value;
-    //     if (rating >= 1 && rating <= 5) {
-    //         alert("Thank you for rating us ${rating}5!");
-    //         ratingInput.value = '';
-    //     } else {
-    //         alert('Please enter a rating between 1 and 5.');
-    //     }
-    // });
 
     submitReviewBtn.addEventListener('click', function () {
         const review = reviewInput.value.trim();
